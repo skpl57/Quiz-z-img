d58f9da2473fb73e0f6b205efc5c556121096aee
skpl57 <kania.skpl57@gmail.com>
2025-10-07 08:07:16 +0200

    widok aplikacji

---
+++ app/src/main/java/com/example/pytania_sk/Pytanie.java
@@ -0,0 +1,11 @@
+package com.example.pytania_sk;
+
+public class Pytanie {
+    private int idObrazek;
+    private String trescPytania;
+    private boolean odpowiedz;
+    private String podpowiedz;
+    private boolean czyOdpOk;
+
+
+}
