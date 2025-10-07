df1044649a2f57f20cb8676dda3f1e09925423ae
skpl57 <kania.skpl57@gmail.com>
2025-10-07 08:56:28 +0200

    gotowe pytania

--- app/src/main/java/com/example/pytania_sk/MainActivity.java
+++ app/src/main/java/com/example/pytania_sk/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.pytania_sk;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<Pytanie> pytania = new ArrayList<>();
+    private Button takB;
+    private Button nieB;
+    private Button nastepneB;
+    private Button podpowiedzB;
+    private TextView pytanieT;
+    private ImageView zdjP;
+    private int numerPytania = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        pytania.add(new Pytanie(R.drawable.szop, "Czy to jest szop pracz?", true, "Pochodzi z rodziny szopowatych."));
+        pytania.add(new Pytanie(R.drawable.szczur, "Czy to Remi?", true, "Jest postacią filmu \"Ratatuj\"."));
+        pytania.add(new Pytanie(R.drawable.kamien, "Czy ten kamień się wstydzi?", true, "Ewidentnie ma czerwone policzki."));
+        pytania.add(new Pytanie(R.drawable.kowadlo, "Czy to kowadło jest zardzewiałe?", false, "Nie ma na nim rdzy."));
+
+        takB = findViewById(R.id.takButton);
+        nieB = findViewById(R.id.nieButton);
+        nastepneB = findViewById(R.id.nastepneButton);
+        podpowiedzB = findViewById(R.id.podpowiedzButton);
+        pytanieT = findViewById(R.id.trescPytania);
+        zdjP = findViewById(R.id.zdjecie);
+        wyswietlPytanie(0);
+
+        nastepneB.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(numerPytania < pytania.size()){
+                            numerPytania++;
+                            wyswietlPytanie(numerPytania);
+                        }
+                    }
+                }
+        );
+    }
+    private void wyswietlPytanie(int ktorePytanie){
+        pytanieT.setText(pytania.get(ktorePytanie).getTrescPytania());
+        zdjP.setImageResource(pytania.get(ktorePytanie).getIdObrazek());
     }
 }
--- app/src/main/java/com/example/pytania_sk/Pytanie.java
+++ app/src/main/java/com/example/pytania_sk/Pytanie.java
@@ -7,5 +7,35 @@ public class Pytanie {
     private String podpowiedz;
     private boolean czyOdpOk;
 
+    public Pytanie(int idObrazek, String trescPytania, boolean odpowiedz, String podpowiedz) {
+        this.idObrazek = idObrazek;
+        this.trescPytania = trescPytania;
+        this.odpowiedz = odpowiedz;
+        this.podpowiedz = podpowiedz;
+        czyOdpOk = false;
+    }
 
+    public void setCzyOdpOk(boolean czyOdpOk) {
+        this.czyOdpOk = czyOdpOk;
+    }
+
+    public int getIdObrazek() {
+        return idObrazek;
+    }
+
+    public String getTrescPytania() {
+        return trescPytania;
+    }
+
+    public boolean isOdpowiedz() {
+        return odpowiedz;
+    }
+
+    public String getPodpowiedz() {
+        return podpowiedz;
+    }
+
+    public boolean isCzyOdpOk() {
+        return czyOdpOk;
+    }
 }
